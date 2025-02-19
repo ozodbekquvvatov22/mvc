f059bae7388ed3b50d1e1dcd172ab0d15d70badd
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-02-19 14:01:00 +0500

    This file for create users

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User
         return $stmt->fetchColumn() > 0;
     }
 
+    
     public function create($name, $email, $password)
     {
         $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
